ded80f47fbbb5170974cedeb14025e6408dbb38f
kapilbhalla <sir.kapil@gmail.com>
2024-06-10 15:07:13 -0700

    Do not show speak button for own chat message
    speak button showings

--- androidKuma/src/main/java/com/example/kumamon/android/Conversation.kt
+++ androidKuma/src/main/java/com/example/kumamon/android/Conversation.kt
@@ -153,9 +153,11 @@ fun ChatBubble(chat: Chat) {
                         color = if (chat.fromUser) Color.White else Color.Black,
                         modifier = Modifier.padding(8.dp)
                     )
-                    // Add a icon to speak the text.
-                    Button(onClick = { speakChatMessage(tts.value, textToSpeak) }) {
-                        Text("Speak")
+                    if (!chat.fromUser) {
+                        // Add a icon to speak the text.
+                        Button(onClick = { speakChatMessage(tts.value, textToSpeak) }) {
+                            Text("Speak")
+                        }
                     }
                 }
             }
--- androidKuma/src/main/java/com/example/kumamon/android/MainActivity.kt
+++ androidKuma/src/main/java/com/example/kumamon/android/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,8 +27,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Conversation(viewModel)
                     Column {
-                        //Conversation(viewModel)
-                        TextToSpeechComponent()
+                        Conversation(viewModel)
                     }
                 }
             }
