79a73890c197d036f15ed1a354afed1d6d0fdfb0
kapilbhalla <sir.kapil@gmail.com>
2024-06-10 15:59:41 -0700

    porting additional properties to common chat data class

--- shared/src/commonMain/kotlin/com/example/kumamon/model/Chat.kt
+++ shared/src/commonMain/kotlin/com/example/kumamon/model/Chat.kt
@@ -1,5 +1,10 @@
 package com.example.kumamon.model
 data class Chat(val message: String = "Hi, I am Kumamon, the lovable black bear mascot of Kumamoto." +
         " My mission is to bring happiness and positivity to everyone I meet." +
-        " If you have any questions about Kumamoto or just need a little pick-me-up, I'm here for you.",
-                val fromUser: Boolean = false)
+        " If you have any questions about Kumamoto or just need a little pick-me-up, I'm here for you." +
+                "Here are a few tips: \n "+
+        "- Translate in japanese \"Hello! good morning \" \n"+
+        "- Show me a picture of ... "
+                ,
+                val fromUser: Boolean = false,
+                val imageUrl: String?=null)
